352643b999d12e84c3dfc5c38bd436a9ef4c81de
liuyinz <cheunghsu@outlook.com>
2021-07-17 15:55:46 +0800

    init : add consult-todo.el

---
+++ consult-todo.el
@@ -0,0 +1,36 @@
+;;; consult-todo.el --- Provide the command `consult-todo' -*- lexical-binding: t -*-
+
+;; Copyright (C) Symbol’s value as variable is void: copyright-current-year 食無魚
+;; Author: 食無魚 <liuyinz@gmail.com>
+;; Created: 2021-07-17 15:53:46
+;; Maintainer: 食無魚
+;; Version: 0.1
+;; Package-Requires: ((emacs "28.0.50") (consult "0.9") (hl-todo "3.1.2"))
+;; Homepage: https://github.com/liuyinz/consult-todo
+;; License: GPL-3.0-or-later
+
+;; This file is not a part of GNU Emacsl.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Usage:
+
+;;; Code:
+
+
+
+(provide 'consult-todo)
+;;; consult-todo.el ends here
