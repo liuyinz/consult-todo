f24dcc87a0d7dd6311617f33bfcf0e4d357af014
liuyinz <liuyinz95@outlook.com>
2025-01-19 19:27:59 +0800

    chore: add more todos

--- consult-todo.el
+++ consult-todo.el
@@ -29,6 +29,9 @@
 
 ;; Provide commands `consult-todo' to search, filter, jump to hl-todo keywords.
 
+;; TODO add cache mechanism for large project
+;; TODO add ignore options for dirs and files
+
 ;;; Code:
 
 (eval-when-compile
